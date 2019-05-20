911eabcd27c3270415275afbe24e09ee4f66d271
Jan Kremen <jan@expan.do>
2019-05-20 14:16:03 +0200

    resolving references

--- test/references.test.ts
+++ test/references.test.ts
@@ -1,5 +1,5 @@
 import { expect } from '@oclif/test';
-import { Identifier, Project,Node, ts, VariableDeclaration } from 'ts-morph';
+import { Identifier, Project, VariableDeclaration } from 'ts-morph';
 
 describe('getting references', function() {
     it('should get declaration from thing', function() {
@@ -10,7 +10,7 @@ describe('getting references', function() {
         let yDeclaration = source.getVariableDeclarationOrThrow('result');
         let identifier: Identifier = yDeclaration.getInitializerOrThrow().getChildren()[0] as any;
         const xDeclaration: VariableDeclaration = identifier.getDefinitions()[0].getDeclarationNode()! as any;
-        xDeclaration.findReferencesAsNodes().forEach(ref=>ref.replaceWithText('5'));
+        xDeclaration.findReferencesAsNodes().forEach(ref=>ref.replaceWithText(xDeclaration.getStructure().initializer as string));
         expect(source.print()).to.be.eq('const x = 5;\nconst result = 5 + 5;\n');
     });
 });
