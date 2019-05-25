77896318d2a26befa78beb602bad5e8c806c4828
yeneek <kremenh@gmail.com>
2019-05-25 19:40:32 +0200

    travis

--- test/commands/new/project.test.ts
+++ test/commands/new/project.test.ts
@@ -4,11 +4,11 @@ import * as rimraf from 'rimraf';
 import { promisify } from 'util';
 
 describe('new:project', () => {
-    test.command(['new:project', './garbage/project'])
+    test.command(['new:project', './.garbage/project'])
         .it('creates project', async ctx => {
-            fs.existsSync('./garbage/project/tsconfig.json');
-            fs.existsSync('./garbage/project/package.json');
-            fs.existsSync('./garbage/project/src/index.ts');
-            await promisify(rimraf)('./garbage/project');
+            fs.existsSync('./.garbage/project/tsconfig.json');
+            fs.existsSync('./.garbage/project/package.json');
+            fs.existsSync('./.garbage/project/src/index.ts');
+            await promisify(rimraf)('./.garbage/project');
         });
 });
